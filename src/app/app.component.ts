de1ce3660c32457f4fefc4b4e23ebcc788d12b07
damnson393 <damnson393@gmail.com>
2022-06-05 01:24:53 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Version } from '@angular/compiler/src/compiler';
 import { Component, VERSION } from '@angular/core';
 
 @Component({
@@ -6,5 +7,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  yoyo = 'Angular'+ VERSION.major;
 }
